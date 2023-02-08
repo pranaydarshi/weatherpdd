5a68f8f13226f87b533d9e9bdb1d222098991a83
pranaydarshi <101990641+pranaydarshi@users.noreply.github.com>
2023-02-08 21:17:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+function weather() {
+
+    let city = document.getElementById("city").value
+    let xhttp = new XMLHttpRequest();
+    let temp = document.getElementById("temp");
+    let xaxis = []
+    let yaxis = []
+    let error = document.getElementById("error")
+    xhttp.onreadystatechange = function () {
+        if (this.status == 404 && this.readyState == 4) {
+            error.innerHTML = `<p style="margin-left: 42%;font-size:20px"> Sorry Incorrect City Name <p>`
+            document.getElementById("temp").innerHTML = "";
+            document.getElementById("graph").innerHTML = "";
+        }
+        else if (this.status == 200 && this.readyState == 4) {
+
+            let data = JSON.parse(xhttp.responseText);
+            temp.innerHTML = `
+
+            <style>
+                table{
+                   
+                }
+            </style>
+
+        <table align="center" cellspacing="">
+        <tr>
+            <td>
+                <p> COUNTRY NAME : ${data.city.country}</p>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <p> MAXIMUM TEMPERATURE : ${data.list[0].main.temp_max} <sup>o</sup</p>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <p> MINIMUM TEMPERATURE : ${data.list[1].main.temp_min} <sup>o</sup></p>
+            </td>
+        </table>
+
+        `
+            for (let i = 0; i < data.list.length; i = i + 8) {
+                let date = data.list[i].dt_txt.split(" ");
+                xaxis.push(date[0])
+                yaxis.push(data.list[i].main.temp)
+
+            }
+            const ctx = document.getElementById('myChart');
+
+            new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: xaxis,
+                    datasets: [{
+                        label: `TEMPERATURE OF ${city.toUpperCase()}`,
+                        data: yaxis,
+                        borderWidth: 1,
+                        backgroundColor: ['red', 'blue', 'orange', 'green', 'purple'],
+                        borderColor: ['black']
+                    }]
+                },
+
+            });
+
+        }
+    }
+    console.log("here")
+    console.log(xaxis)
+    xhttp.open("GET", `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=e15c55a23793c294908375f6b427d0f0&units=metric`, false);
+    xhttp.send();
+
+
+}
+
+
